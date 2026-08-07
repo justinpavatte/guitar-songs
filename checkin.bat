db4d9f89c17ba1c39cd80dd0deeb1ce0d3d49399
Justin <justinpavatte@gmail.com>
2026-08-07 10:30:47 -0500

    Generic check in.

--- checkin.bat
+++ checkin.bat
@@ -1,14 +1,6 @@
-@echo off
 cd /d C:\code\guitar-songs
-
-set "COMMENT="
-set /p "COMMENT=Commit comment (Enter for Generic check in.): "
-
-if not defined COMMENT set "COMMENT=Generic check in."
-
 git add .
-git commit -m "%COMMENT%"
+git commit -m "Generic check in."
 git push
 ssh ha "cd /srv/www/guitar-songs && git pull"
-
 pause
