d7e0773917b3f8ff01e6747b1866badcc8e1d51e
Justin <justinpavatte@gmail.com>
2026-08-07 13:00:57 -0500

    Generic check in.

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const APP_CACHE = "song-reference-app-v1";
+const APP_CACHE = "song-reference-app-v2";
 const PDF_CACHE = "song-reference-pdfs-v1";
 const API_CACHE = "song-reference-api-v1";
 
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=2"
+  "./styles.css?v=4"
 ];
 
 self.addEventListener("install", event => {
